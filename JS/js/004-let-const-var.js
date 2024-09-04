089560954f64bca76d8fb1d9ef146f0cb8a35f9f
Edip Mangtay <mangtay0133@gmail.com>
2024-09-04 10:55:11 +0300

    boolean

--- JS/js/004-let-const-var.js
+++ JS/js/004-let-const-var.js
@@ -1,21 +1,65 @@
-let user1 = "1. user"
-console.log(user1);
-console.log
-(   
-    "2. user"
+// rakamla degisken isimleri baslayamaz..
+
+// let 1user = "1. user"
+// console.log(1user)
+
+// console.log(user1)
+
+;let user1 = "1. user"
+;console.log(user1);
+console.log(
+  user1
 );
+// Not: Her kodun basinda aslinda gorunmeyen bir noktalivirgul var..
+// ama yinede biz her satirin basina noktalivirgul eklemiyoruz :)
 
 let user;
-console.log(user); // undefined
-user = "Mangtay"; // değişiklik yapıldı 
-console.log(user); // Mangtay
+console.log(user)
 
-const password = 1234; // const keywordü ile sabit bir değişken tanımlanır
-console.log(password); // 1234
-//Conts kullanarak bir kez tenaımlama yapılabilir
-// password = 5678; // TypeError: Assignment to constant variable
-//blok içerisinde tekrar aynı isimler kullanılaiblir 
+// let user = "Sistem Kullanici"
+user = "SystemUser"
+
+user = "Admin"
+console.log(user)
+
+const PASSWORD = 1234;
+console.log("Sifre:", PASSWORD)
+
+
+// bu hatali cunku const ile bir defa tanimlanabilir..
+// PASSWORD = "100123432423523"
+// console.log("Degisen Sifre:", PASSWORD)
+
+// Blok icinde tekrar ayni isimler kullanilabilir..
 {
-    const password = 5678; // buradaki conts password farklı çünkü farklı blok içerisinde yer alıyor 
-    console.log(password); // 5678
+  let user = "Duygu";
+  console.log("Blok icinde:", user)
+  const PASSWORD = "12143-0583409583453405983049534957"
+  console.log("Blok icinde:", PASSWORD)
 }
+
+console.log(user)
+console.log("Sifre:", PASSWORD)
+
+console.log("44. Satidaki Bilgiye Erisebilir miyiz? ", userName)
+
+var userName = "44. Satirdaki Bilgi"
+console.info("userName:", userName)
+
+{
+  userName = "Blok Icindeki Bilgi"
+  console.info("userName Blok:", userName)
+}
+
+// Degisken isimlerinin lutfen anlamli olmasini saglayalim..
+let i = "1" // bunu kullanmayalim
+let x = "info" // bunu kullanmayalim
+
+// bool -> laptop kullaniyor musun? evet / hayir 
+// macOs kullaniyormusun? evet / hayir
+// yazilimci misin? evet / hayir
+
+//BOOL
+isMacOsUser = true 
+isLaptopUser = false
+hasPassword = true
