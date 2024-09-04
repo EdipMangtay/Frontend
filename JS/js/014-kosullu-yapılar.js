5c2b9724a94ea3a32f25f7b500f7f137268cb99b
Edip Mangtay <mangtay0133@gmail.com>
2024-09-04 14:44:17 +0300

    detaylı kosul yapıları

--- JS/js/014-kosullu-yapılar.js
+++ JS/js/014-kosullu-yapılar.js
@@ -1,9 +1,41 @@
-const userID = prompt("TC:  ");
-const userAge = prompt("Yaşınızı giriniz: ");
-if (userAge >= 18 && userID.length === 11) {  // burada 3 tane = kullandık çünkü hem değer hem de tip kontrolü yapmak istiyoruz.
-    alert("Sisteme giriş yapabilirsiniz.");
+const userId = prompt("TC No Giriniz: ")
+const userAge = prompt("Yasinizi Giriniz: ")
+
+// not: If her zaman true'da calisir..
+// if (true) {console.log('calisti..')}
+// if (false) {console.log('calismaz')}
+// if (!false) {console.log('calisir')}
+
+// TC length 11 olacak
+// 12345678900
+
+// negatiften pozitife..
+// if (userId.length !== 11) { 
+//   console.log("TC No Bilgisi Dogru Olmayabilir..")
+// } else if (userId.length === 11 && userAge < 18) {
+//   console.log("Siteye Giremezsiniz Cunku 18 Yasindan Kucuksun..")
+// } else if (userId.length === 11 && userAge >= 18) {
+//   console.log("Siteye Girebilirsiniz..")
+// }
+
+// pozitiften negatife..
+
+if (userId.length === 11 && userAge >= 18 && userAge <= 99) {
+  const h1TitleElement = document.querySelector('h1#title')
+  h1TitleElement.innerHTML = '<em>Siteye Girebilirsiniz..</em>'
+  // h1TitleElement.innerText = '<em>Siteye Girebilirsiniz..</em>'
+  // if (userAge > 99) {
+  //   console.log("Yas Bilgisi Hatali..") // acaba burda olmasi dogr muydu ?
+  // }
+  // console.log("Siteye Girebilirsiniz..")
+} else if (!userAge <= 99) { 
+  console.log("Yas Bilgisi 99dan buyuk olabilir..")
+  // tersini al operatorunu kullandik ;)
+} else if (userAge > 99) { 
+  console.log("Yas Bilgisi 99dan buyuk olabilir..")
+} else if (userId.length !== 11) { 
+  console.log("TC No Bilgisi Dogru Olmayabilir..")
 }
-else {
-    alert("Giriş yapamazsın")
-    window.location.replace("https://www.google.com");
+else if (userId.length === 11 && userAge < 18) {
+  console.log("Siteye Giremezsiniz Cunku 18 Yasindan Kucuksun..")
 }
