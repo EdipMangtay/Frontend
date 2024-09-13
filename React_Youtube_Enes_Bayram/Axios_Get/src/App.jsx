fd10326f60cd66c91883d88bae0b3dd9cfdb66a9
Edip Mangtay <mangtay0133@gmail.com>
2024-09-13 18:37:03 +0300

    axois

---
+++ React_Youtube_Enes_Bayram/Axios_Get/src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const BASE_URL = 'http://localhost:3005'
+
+function App() {
+
+  const getAllUsers = async()=>{
+     const response = await axios.get(BASE_URL + '/users') // Ben seni istedim sen gelince diğer kodları çalıştıracağım dedik
+     console.log(response.data)
+  }
+  const getUserById = async(userid) =>{
+    const response = await axios.get(BASE_URL + '/users/'+userid);
+    console.log(response.data)
+  }
+
+
+  const createUser = async(newUser)=>{
+    // Post Veri eklemek için kullanılır
+    const response = await axios.post(`${BASE_URL}/users`,newUser);
+    console.log("response",response.data)
+  }
+  const updateUser = async(userId,updatedUser)=>{
+  // Put Veri güncellemek için kullanılır
+    await axios.put(`${BASE_URL}/users/${userId}`,updatedUser);
+  }
+  const deleteUser = async(userId) =>{
+    // Delete veri silmek için kullanılır
+    
+    const deleted = await axios.delete(`${BASE_URL}/users/${userId}`);
+    console.log(deleted);
+  }
+
+
+  useEffect(() =>{
+    // getAllUsers()
+    // getUserById(1)
+    // const newUser={
+    //   "username":"ahmet",
+    //   "password":"1234",
+    // }
+    // createUser(newUser)
+
+    // updateUser(1,{
+    //   "username":"Mavi",
+    //   "password":"5660",})
+    deleteUser("8569");
+
+  },[])
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
